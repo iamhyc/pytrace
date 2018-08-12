6c5c7be792020b587cc54c00254b2a194b632ac8
iamhyc <sudofree@163.com>
2018-08-12 17:20:18 +0800

    finish basic function

--- pytrace.py
+++ pytrace.py
@@ -9,6 +9,7 @@ def main(args):
     events = [('-e', x) for x in args.events]
     events = [x for xs in events for x in xs]
     cmdl = ['trace-cmd', 'record'] + events + ['-o', TMP_FILE]
+    result = list()
 
     with Popen(cmdl, stdout=SP_PIPE) as proc:
         input('[Recording] press ENTER to stop...')
@@ -16,8 +17,25 @@ def main(args):
         pass
     
     with Popen(['trace-cmd', 'report', TMP_FILE], stdout=SP_PIPE) as proc:
-        print(proc.stdout.read())
+        for line in proc.stdout.readlines():
+            tmp = line.split(maxsplit=4)
+            if len(tmp) == 5:
+                tmp[2] = float(tmp[2][:-1]) #get float timestamp
+                tmp[3] = tmp[3][:-1] # remove column
+                result.append(tuple(tmp))
+                pass
+            pass
         pass
+
+    if args.delta_flag:
+        for x in range(1, len(result)):
+            print( '%f %f'%(result[x-1][2], result[x][2]-result[x-1][2]) )
+        pass
+    
+    if args.show_flag:
+        print('Not implement yet...')
+        return
+
     pass
 
 if __name__ == '__main__':
@@ -26,8 +44,10 @@ if __name__ == '__main__':
             description='A customed python wrapper for trace-cmd.')
         parser.add_argument('events', nargs='+', type=str,
             help='events to be traced.')
-        parser.add_argument('--delta', dest='delta_flag', action='store_true', default=True,
+        parser.add_argument('--delta', dest='delta_flag', action='store_true', default=False,
             help='analyze delta time between events, and output.')
+        parser.add_argument('--show', dest='show_flag', action='store_true', default=False,
+            help='analyze output with kernelshark.')
         args = parser.parse_args()
 
         main(args)
