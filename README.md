33a61183be9619703982e8f047fe9b485961fee8
iamhyc <sudofree@163.com>
2022-06-08 21:55:56 +0800

    restart

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ A little tool for ftrace recording events group-analysis and time-analysis.
 
 ### Demo
 
-(This is a **transmission interval analysis** demo for *iwlwifi* module, intel wireless NIC)
+(This is a **transmission interval analysis** demo for *iwlwifi* module of intel wireless NIC)
 
-![demo-00](./demo-00.gif)
+![demo-00](./demo.gif)
--- pytrace.py
+++ pytrace.py
@@ -1,109 +1,114 @@
 #!/usr/bin/python3
-import sys, time, signal, threading, argparse
-import queue, threading
-from subprocess import Popen, PIPE as SP_PIPE
+import os, re, time, signal
+import argparse
+import subprocess as sp
+from pathlib import Path
+from termcolor import cprint
 
-TMP_FILE='/tmp/data-pytrace.dat'
+TMP_FILE = '/tmp/pytrace-%s.dat'%( time.strftime('%Y%m%d-%H%M%S') )
+TRACE_SLOT_US = 500 #default as 1000us, i.e., 1ms.
 
-def startThread(target, args=()):
-    args = tuple(args)
-    t = threading.Thread(target=target, args=args)
-    t.setDaemon(True)
-    t.start()
-    return t
+SHELL_RUN  = lambda x: sp.run(x, stdout=sp.PIPE, stderr=sp.PIPE, check=True, shell=True)
+TRACE_ITEM = re.compile( '\s+(\S+)\s+\[(\d+)\] (\d+\.\d+):\s+(\S+):\s+(.+)' )
+#                            [task,    cpu,    timestamp,    event,   info]
 
-def execThread(proc_cmdl, q):
-    if not proc_cmdl: return
-    
-    with Popen(proc_cmdl.split(), stderr=SP_PIPE) as proc:
-        q.get() #block here
-        proc.terminate()
-        pass
-    pass
+def record_events(events, exec_cmd=None):
+    events = [['-e', x] for x in events]
+    events = sum(events,[]) #flatten
+    trace_cmd = ['trace-cmd', 'record', '-s', str(TRACE_SLOT_US), '-o', TMP_FILE] + events
+    if os.geteuid() != 0:
+        trace_cmd = ['sudo'] + trace_cmd
+        SHELL_RUN('sudo echo')
 
-def recordProcedure(args):
-    q = queue.Queue()
-    events = [('-e', x) for x in args.events]
-    events = [x for xs in events for x in xs]
-    cmdl = ['trace-cmd', 'record'] + events + ['-s', '100', '-o', TMP_FILE]
+    ## start trace-cmd and wait for initialized
+    trace_proc = sp.Popen(trace_cmd, stdout=sp.PIPE, stderr=sp.PIPE)
+    cprint('Initializing, Please Wait ...\n', 'green')
+    time.sleep(2.0)
 
-    with Popen(cmdl, stdout=SP_PIPE, stderr=SP_PIPE) as proc:
-        print('============ Initializing, Please Wait ============\n')
-        time.sleep(3.0)
+    ## execute at the same time
+    if exec_cmd:
+        exec_proc = sp.Popen(exec_cmd, shell=True)
 
-        t = startThread(execThread, args=(args.exec_proc, q))
-        input('========== Recording, press ENTER to stop ==========\n')
-        proc.send_signal(signal.SIGINT)
-        q.put_nowait('exit')
-
-        print('============= Processing, Please Wait =============\n')
+    ## hook SIGINT signal
+    def signal_handler(sig, frame):
+        try:
+            trace_proc.send_signal(signal.SIGINT)
+            exec_proc.terminate()
+        except:
+            pass
+        cprint('\nProcessing, Please Wait ...', 'green')
         time.sleep(5.0)
         pass
+    signal.signal(signal.SIGINT, signal_handler)
     
+    ## wait until trace terminated
+    trace_proc.wait()
+    return TMP_FILE
+
+def filter_events(trace_file, filters):
+    raw_records    = SHELL_RUN( f'trace-cmd report -i {trace_file}' ).stdout.decode()
+    cooked_records = TRACE_ITEM.findall(raw_records)
+
+    if not filters:
+        return cooked_records
+
+    results = {name:[] for name in filters}
+    for item in cooked_records:
+        for name in filters:
+            if name in item[-1]:
+                print(item)
+                results[name].append( item )
+        pass
+
+    return results
+
+def analyze(events):
     pass
 
 def main(args):
-    result = list()
-    file_name = '+'.join(args.events)
-
-    if not args.strike_flag:
-        recordProcedure(args)
-
-    with Popen(['trace-cmd', 'report', TMP_FILE], stdout=SP_PIPE) as proc:
-        for line in proc.stdout.readlines():
-            tmp = line.split(maxsplit=4)
-            if len(tmp)==5 and (args.filter in tmp[4].decode()): #TODO: impl. regex for filter
-                tmp[2] = float(tmp[2][:-1]) #get float timestamp
-                tmp[3] = tmp[3][:-1] #remove column
-                result.append(tuple(tmp))
-                pass
-            pass
-        pass
-
-    if len(result) != 0:
-        with open(file_name+'.log', 'w+') as fd:
-            if args.output_flag:
-                [fd.write('%f\n'%x[2]) for x in result]
-                pass
-            elif args.delta_flag:
-                for x in range(1, len(result)):
-                    fd.write('%f %f\n'%(result[x-1][2], result[x][2]-result[x-1][2]))
-                pass
-            else: #vomit on the console
-                [print('%s %s %.6f %s %s'%(
-                    x[0].decode(), x[1].decode(), x[2], x[3].decode(), x[4].decode())) for x in result]
-                pass
-            pass
-        pass
-
-    if args.show_flag:
-        Popen(['kernelshark', '-i', TMP_FILE])
+    ## get trace file, w/ program executed at same time
+    if args.command=='record':
+        _file = record_events(args.events, args.exec)
+    elif args.command=='replay':
+        if args.trace_file:
+            _file = args.trace_file
+        else:
+            _all  = sorted( Path('/tmp').glob('pytrace-*.dat') )
+            _file = _all[-1] #the last trace file
+    else:
         return
+
+    ## filter the recorded events
+    events = filter_events(_file, args.filters)
+
+    ## customized analysis
+    analyze(events)
 
     pass
 
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser(
-            description='A customed python wrapper for trace-cmd.')
-        parser.add_argument('events', nargs='+', type=str,
-            help='events to be traced.')
-        parser.add_argument('--exec', dest='exec_proc', default='',
-            help='execute process when recording begins.')
-        parser.add_argument('--filter', dest='filter', type=str, default='',
-            help='filter the captured events by info section.')
-        parser.add_argument('--redo', dest='strike_flag', action='store_true', default=False,
-            help='use the last recorded data.')
-        parser.add_argument('--output', dest='output_flag', action='store_true', default=False,
-            help='output the raw timestamp data.')
-        parser.add_argument('--delta', dest='delta_flag', action='store_true', default=False,
-            help='analyze delta time between events, and output.')
-        parser.add_argument('--show', dest='show_flag', action='store_true', default=False,
-            help='analyze output with kernelshark.')
-        args = parser.parse_args()
+                                description='A customized python wrapper for trace-cmd.')
+        subparsers = parser.add_subparsers(dest='command')
+        ##
+        p_record = subparsers.add_parser('record', help='record new events to analyze.')
+        p_record.add_argument('events', nargs='+', type=str,
+                                help='events to be traced.')
+        p_record.add_argument('--exec', dest='exec', default='',
+                                help='execute process when recording begins.')
+        p_record.add_argument('--filter', dest='filters', nargs='*', type=str,
+                                help='filter the captured events by info section.')
+        ##
+        p_replay = subparsers.add_parser('replay', help='replay existing trace file to analyze.')
+        p_replay.add_argument('trace_file', nargs='?', type=str,
+                                help='events to be traced.')
+        p_replay.add_argument('--filter', dest='filters', nargs='*', type=str,
+                                help='filter the captured events by info section.')
 
+        args = parser.parse_args()
         main(args)
     except Exception as e:
-        print(e)
+        raise e
     finally:
-        exit()
+        pass
